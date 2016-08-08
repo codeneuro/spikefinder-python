335c12d91add8374646c92da9035b86b8e07d4a1
pberens <pberens>
2016-08-08 15:58:16 +0200

    spearman, info, like

--- spikefinder/main.py
+++ spikefinder/main.py
@@ -1,7 +1,14 @@
-import os
-import json
+
 from scipy import corrcoef
+from scipy.stats import spearmanr
+from numpy import percentile, asarray, arange, zeros, where, repeat, sort, cov, mean, std, ceil
+from numpy import vstack, hstack, argmin, ones, convolve, log, linspace, min, max, square, sum, diff
+from numpy import array, eye, dot, empty, seterr, isnan, any, zeros_like
+from scipy.optimize import minimize
+from scipy.stats import poisson
+from scipy.interpolate import interp1d
 from pandas import read_csv
+from warnings import warn
 
 def load(file):
     """
@@ -34,13 +41,150 @@ def _corr(x, y):
     return corrcoef(x, y)[0,1]
 
 def _info(x, y):
-    pass
+    loglik, info = _infolik(x,y)
+    return info       # this is absolute info gain, we report mean(info)/mean(entropy) across all cells in the paper
 
 def _rank(x, y):
-    pass
+    
+    return spearmanr(x,y,nan_policy='raise').correlation        
 
 def _auc(x, y):
-    pass
+     pass
 
 def _loglik(x, y):
-    pass
+    
+    loglik, info = _infolik(x,y)
+    return loglik
+
+def _downsample(signal, factor):
+    """
+    Downsample signal by averaging neighboring values.
+    @type  signal: array_like
+    @param signal: one-dimensional signal to be downsampled
+    @type  factor: int
+    @param factor: this many neighboring values are averaged
+    @rtype: ndarray
+    @return: downsampled signal
+    """
+
+    if factor < 2:
+        return asarray(signal)
+        
+    return convolve(asarray(signal).ravel(), ones(factor), 'valid')[::factor]
+    
+    
+def _infolik(spikes,predictions):
+    ''' 
+    Computes log likelihood of the data and the information gain
+    
+    adapted from lucas theis, c2s package
+    https://github.com/lucastheis/c2s   
+        
+    '''
+    
+    fps = 100    ### THIS OF COURSE HAS TO PASSED SOMEHOW
+    factor = 1   ### DITO
+    
+    # find optimal point-wise monotonic function
+    f = optimize_predictions(predictions,spikes, num_support=10, regularize=5e-8, verbosity=2)
+
+    # for conversion into bit/s
+    factor = 1. / factor / log(2.)     
+
+    # average firing rate (Hz) over all cells
+    firing_rate = mean(spikes) * fps;
+
+    # estimate log-likelihood and marginal entropies
+    loglik = mean(poisson.logpmf(spikes, f(predictions))) * fps * factor
+    entropy= -mean(poisson.logpmf(spikes, firing_rate / fps)) * fps * factor
+
+    return loglik, loglik + entropy
+    
+    
+    
+def optimize_predictions(predictions, spikes, num_support=10, regularize=5e-8, verbosity=1):
+    """
+    Fits a monotonic piecewise linear function to maximize the Poisson likelihood of
+    firing rate predictions interpreted as Poisson rate parameter.
+    @type  predictions: array_like
+    @param predictions: predicted firing rates
+    @type  spikes: array_like
+    @param spikes: true spike counts
+    @type  num_support: int
+    @param num_support: number of support points of the piecewise linear function
+    @type  regularize: float
+    @param regularize: strength of regularization for smoothness
+    @rtype: interp1d
+    @return: a piecewise monotonic function
+    
+    adapted from lucas theis, c2s package
+    https://github.com/lucastheis/c2s
+    """
+
+    if num_support < 2:
+        raise ValueError('`num_support` should be at least 2.')
+
+    if any(predictions < 0.):
+        warn('Some firing rate predictions are smaller than zero.')
+        predictions[predictions < 0.] = 0.
+
+    if any(isnan(predictions)):
+        warn('Some predictions are NaN.')
+        predictions[isnan(predictions)] = 0.
+
+    # support points of piece-wise linear function
+    if num_support > 2:
+        F = predictions
+        F = F[F > (max(F) - min(F)) / 100.]
+        x = list(percentile(F, range(0, 101, num_support)[1:-1]))
+        x = asarray([0.] + x + [max(F)])
+
+        for i in range(len(x) - 1):
+            if x[i + 1] - x[i] < 1e-6:
+                x[i + 1] = x[i] + 1e-6
+    else:
+        x = asarray([min(predictions), max(predictions)])
+
+        if x[1] - x[0] < 1e-6:
+                x[1] += 1e-6
+
+    def objf(y):
+        # construct piece-wise linear function
+        f = interp1d(x, y)
+
+        # compute predicted firing rates
+        l = f(predictions) + 1e-16
+
+        # compute negative log-likelihood (ignoring constants)
+        K = mean(l - spikes * log(l))
+
+        # regularize curvature
+        z = (x[2:] - x[:-2]) / 2.
+        K = K + regularize * sum(square(diff(diff(y) / diff(x)) / z))
+
+        return K
+
+    class MonotonicityConstraint:
+        def __init__(self, i):
+            self.i = i
+
+        def __call__(self, y):
+            return y[self.i] - y[self.i - 1]
+
+    # monotonicity and non-negativity constraint
+    constraints = [{'type': 'ineq', 'fun': MonotonicityConstraint(i)} for i in range(1, x.size)]
+    constraints.extend([{'type': 'ineq', 'fun': lambda y: y[0]}])
+
+    # fit monotonic function
+    settings = seterr(invalid='ignore')
+    res = minimize(
+		fun=objf,
+		x0=x + 1e-6,
+		method='SLSQP',
+		tol=1e-9,
+		constraints=constraints,
+		options={'disp': 1, 'iprint': verbosity})
+    seterr(invalid=settings['invalid'])
+
+	# construct monotonic piecewise linear function
+    return interp1d(x, res.x, bounds_error=False, fill_value=res.x[-1])
